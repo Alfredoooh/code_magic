64a47ab4926186a3ed99360d595f80f36414e6fb
Alfredoooh <alfredopjonas@gmail.com>
2025-10-06 14:47:25 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,15 +5,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir = file("../build")
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    afterEvaluate {
+        val newSubprojectBuildDir = File(newBuildDir, project.name)
+        project.layout.buildDirectory.set(newSubprojectBuildDir)
+    }
 }
 
 tasks.register<Delete>("clean") {
