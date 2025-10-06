c2e21b2ac24fe65d69696aa620fc723f3688655a
Alfredoooh <alfredopjonas@gmail.com>
2025-10-06 14:56:28 +0100

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,28 @@
 pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 plugins {
-    id "com.android.application" version "8.1.1" apply false
-    id "com.android.library" version "8.1.1" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.10" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    // Atualizado para suportar Flutter >= 3.22
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    // Kotlin compatível
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+
+include(":app")
