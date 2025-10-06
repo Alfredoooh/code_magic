f5d3f5e1151024db36259352738254639366ba6a
Alfredoooh <alfredopjonas@gmail.com>
2025-10-06 14:58:20 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+import org.gradle.api.tasks.Delete
+
+// Versão Kotlin compatível com o settings.gradle.kts anterior
+val kotlinVersion = "1.9.10"
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +20,7 @@ allprojects {
     }
 }
 
+// Mantém o build output fora do diretório default do projecto
 val newBuildDir = file("../build")
 rootProject.layout.buildDirectory.set(newBuildDir)
 
