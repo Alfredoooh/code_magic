d9df9d9cda237169c9676fd31daa61b5bc218f2d
Alfredoooh <alfredopjonas@gmail.com>
2025-10-07 17:06:38 +0100

    Update auth_gate.dart

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,10 +1,11 @@
-// auth_gate.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login_screen.dart';
 import 'main_screen.dart';
 
 class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key); // ADICIONADO construtor const
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
