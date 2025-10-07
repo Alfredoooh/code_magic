60d69f98b8bd801f84b04d2e03d94fb75a30b338
Alfredoooh <alfredopjonas@gmail.com>
2025-10-07 14:10:39 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'screens/auth_gate.dart';
 import 'screens/main_screen.dart';
 import 'services/theme_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await ThemeService.init();
-  runApp(ChatApp());
+  // força statusbar style compatível com tema
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarBrightness: ThemeService.isDarkMode ? Brightness.dark : Brightness.light));
+  runApp(const ChatApp());
+}
+
+/// Global scroll behaviour para bounce estilo iOS
+class BouncingScrollBehavior extends MaterialScrollBehavior {
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) => const BouncingScrollPhysics();
 }
 
 class ChatApp extends StatefulWidget {
+  const ChatApp({Key? key}) : super(key: key);
   @override
   State<ChatApp> createState() => _ChatAppState();
 }
@@ -30,18 +41,26 @@ class _ChatAppState extends State<ChatApp> {
   }
 
   void _onThemeChanged() {
-    setState(() {});
+    if (mounted) setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Easify',
-      theme: ThemeService.getThemeData(),
-      home: AuthGate(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeService.getThemeData(),
+      scrollBehavior: BouncingScrollBehavior(), // iOS-like scrolling
+      home: const AuthGate(),
       routes: {
         '/main': (context) => MainScreen(),
+      },
+      // Força uso de Cupertino-style overlays (more iOS look)
+      builder: (context, child) {
+        return AnnotatedRegion<SystemUiOverlayStyle>(
+          value: ThemeService.isDarkMode ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
+          child: child ?? const SizedBox.shrink(),
+        );
       },
     );
   }
