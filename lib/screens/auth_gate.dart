64bbe7e6c4220948b745055f61a7fd192ea7f799
Alfredoooh <alfredopjonas@gmail.com>
2025-10-07 01:38:41 +0100

    Create auth_gate.dart

---
+++ lib/screens/auth_gate.dart
@@ -0,0 +1,20 @@
+// auth_gate.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_screen.dart';
+import 'main_screen.dart';
+
+class AuthGate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return MainScreen();
+        }
+        return LoginScreen();
+      },
+    );
+  }
+}
