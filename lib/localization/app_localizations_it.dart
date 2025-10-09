fd2a90bbcb2bc6f74bd9843fbcfa0fc99062c977
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 00:29:22 +0100

    Atualizar o app_localizations_it.dart

--- lib/localization/app_localizations_it.dart
+++ lib/localization/app_localizations_it.dart
@@ -196,7 +196,6 @@ const Map<String, String> it = {
   'terms': 'Termini',
   'privacy': 'Privacy',
   'contact': 'Contatto',
-  'support': 'Supporto',
   'feedback': 'Feedback',
   'rate_app': 'Valuta app',
   'update': 'Aggiorna',
