aa726a34dae228499064ac57293029245a6d5bfe
Alfredoooh <alfredopjonas@gmail.com>
2025-10-09 12:29:46 +0100

    Atualizar o auth_gate.dart

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login_screen.dart';
-import 'main_screen.dart';
+import 'home_screen.dart';
 
 class AuthGate extends StatelessWidget {
-  const AuthGate({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return const MainScreen();
+          return HomeScreen();
         }
-        return const LoginScreen();
+        return LoginScreen();
       },
     );
   }
