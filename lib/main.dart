0395ef43b3ef2bee9d2dafdadcc3d5de37299c51
Alfredoooh <alfredopjonas@gmail.com>
2025-10-09 12:29:12 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/auth_gate.dart';
-import 'screens/main_screen.dart';
-import 'services/theme_service.dart';
-import 'services/user_data_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await ThemeService.init();
-  await UserDataService.init();
-  runApp(const ChatApp());
+  runApp(ChatApp());
 }
 
-class ChatApp extends StatefulWidget {
-  const ChatApp({Key? key}) : super(key: key);
-
-  @override
-  State<ChatApp> createState() => _ChatAppState();
-}
-
-class _ChatAppState extends State<ChatApp> {
-  @override
-  void initState() {
-    super.initState();
-    ThemeService.addListener(_onThemeChanged);
-  }
-
-  @override
-  void dispose() {
-    ThemeService.removeListener(_onThemeChanged);
-    super.dispose();
-  }
-
-  void _onThemeChanged() {
-    setState(() {});
-  }
-
+class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Easify',
-      theme: ThemeService.currentThemeData,
+      title: 'Chat App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: Color(0xFFFF8C42),
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFFFF8C42),
+          secondary: Color(0xFFFF8C42),
+        ),
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+      home: AuthGate(),
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
-      routes: {
-        '/main': (context) => const MainScreen(),
-      },
     );
   }
 }
