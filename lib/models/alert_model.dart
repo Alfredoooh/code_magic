0d56f21c6431e2c1d137d3ce1b9a8e6dc67d8ac0
Alfredoooh <alfredopjonas@gmail.com>
2025-10-09 15:28:19 +0100

    Create alert_model.dart

---
+++ lib/models/alert_model.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AlertModel {
+  final String id;
+  final String userId;
+  final String asset;
+  final String condition;
+  final bool active;
+  final Timestamp lastTriggered;
+
+  AlertModel({
+    required this.id,
+    required this.userId,
+    required this.asset,
+    required this.condition,
+    this.active = true,
+    required this.lastTriggered,
+  });
+
+  factory AlertModel.fromJson(Map<String, dynamic> json) {
+    return AlertModel(
+      id: json['id'],
+      userId: json['userId'],
+      asset: json['asset'],
+      condition: json['condition'],
+      active: json['active'] ?? true,
+      lastTriggered: json['lastTriggered'] as Timestamp,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'asset': asset,
+      'condition': condition,
+      'active': active,
+      'lastTriggered': lastTriggered,
+    };
+  }
+}
