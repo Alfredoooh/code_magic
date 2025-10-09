491ed40e6e4b7c5899d64cc32a2f4fb4928c9d1e
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 00:28:44 +0100

    Atualizar o app_localizations_fr.dart

--- lib/localization/app_localizations_fr.dart
+++ lib/localization/app_localizations_fr.dart
@@ -196,7 +196,6 @@ const Map<String, String> fr = {
   'terms': 'Conditions',
   'privacy': 'Confidentialité',
   'contact': 'Contact',
-  'support': 'Support',
   'feedback': 'Commentaires',
   'rate_app': 'Évaluer l\'application',
   'update': 'Mettre à jour',
