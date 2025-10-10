054fad15ea0f1858b53ce8d0b8fa400b3943fc01
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 12:33:03 +0100

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,35 +1,58 @@
-import org.gradle.api.tasks.Delete
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+android {
+    namespace = "com.nexa.madeeasy"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.nexa.madeeasy"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+        multiDexEnabled = true
     }
-    dependencies {
-        // Atualizado para Kotlin 2.1.0 (combina com bibliotecas compiladas com Kotlin 2.1)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-        // plug-in do Google Services para Firebase
-        classpath("com.google.gms:google-services:4.4.3")
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+flutter { source = "../.." }
 
-val newBuildDir = file("../build")
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    afterEvaluate {
-        val newSubprojectBuildDir = File(newBuildDir, project.name)
-        project.layout.buildDirectory.set(newSubprojectBuildDir)
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("androidx.core:core-ktx:1.12.0")
+    // alinhar com o plugin Kotlin 2.1.0
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+    implementation("androidx.multidex:multidex:2.0.1")
 }
