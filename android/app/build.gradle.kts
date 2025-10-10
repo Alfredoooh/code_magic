8f35fa33c4b7d8e91b473c6e2a2a6efb6a3c7669
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 12:39:59 +0100

    Atualizar o build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,28 +1,29 @@
+import org.gradle.api.tasks.Delete
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
-    // Aplica o plug-in do Google Services
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.nexa.madeeasy"
-    compileSdk = 36
+    namespace "com.nexa.madeeasy"
+    compileSdk 36
 
     defaultConfig {
-        applicationId = "com.nexa.madeeasy"
-        minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
-        multiDexEnabled = true
+        applicationId "com.nexa.madeeasy"
+        minSdk 21
+        targetSdk 36
+        versionCode 1
+        versionName "1.0.0"
+        multiDexEnabled true
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility JavaVersion.VERSION_11
+        targetCompatibility JavaVersion.VERSION_11
+        coreLibraryDesugaringEnabled true
     }
 
     kotlinOptions {
@@ -30,10 +31,10 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            signingConfig = signingConfigs.getByName("debug")
+        release {
+            minifyEnabled false
+            shrinkResources false
+            signingConfig signingConfigs.debug
         }
     }
 
@@ -50,19 +51,13 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    
-    
+
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-    
-    
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-    
-    
+
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
     implementation("androidx.multidex:multidex:2.0.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.25")
 }
-
-
