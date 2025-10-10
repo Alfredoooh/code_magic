71346b2b51fb967a2a857c9e27db5f502a1edbb0
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 16:24:54 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,17 +10,14 @@ pluginManagement {
     }
 
     plugins {
-        // Versões centralizadas de plugins usados no projeto
         id("com.android.application") version "8.1.1"
         id("org.jetbrains.kotlin.android") version "1.9.22"
         id("com.google.gms.google-services") version "4.4.2"
-        // O plugin do Flutter é fornecido pela toolchain do Flutter; não precisa versão aqui.
-        id("dev.flutter.flutter-gradle-plugin") apply false
+        // REMOVIDO: id("dev.flutter.flutter-gradle-plugin")
     }
 }
 
 dependencyResolutionManagement {
-    // Removemos repositoriesMode para compatibilidade com diferentes versões do Gradle
     repositories {
         google()
         mavenCentral()
