dd25be0cb1057e2dae0ae641bd7f50da1598d8e6
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 12:43:41 +0100

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,58 +1,31 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.nexa.madeeasy"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.nexa.madeeasy"
-        minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
-        multiDexEnabled = true
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.google.gms:google-services:4.4.3")
     }
 }
 
-flutter { source = "../.." }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("androidx.core:core-ktx:1.12.0")
-    // alinhar com o plugin Kotlin 2.1.0
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
-    implementation("androidx.multidex:multidex:2.0.1")
+def newBuildDir = file("../build")
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    afterEvaluate {
+        def newSubprojectBuildDir = new File(newBuildDir, project.name)
+        project.layout.buildDirectory.set(newSubprojectBuildDir)
+    }
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.layout.buildDirectory
 }
