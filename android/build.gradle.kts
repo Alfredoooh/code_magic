8cc16baccecbe4c2245faf8664c2e22516df99b0
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 15:31:05 +0100

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,20 @@
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
+// 🔥 Adiciona o buildscript para o Firebase funcionar
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Plugin Google Services (essencial para Firebase)
+        classpath("com.google.gms:google-services:4.4.2")
+        // Kotlin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +22,7 @@ allprojects {
     }
 }
 
+// 🔧 Reorganiza diretórios de build (seu código original)
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,10 +30,12 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 🧹 Tarefa de limpeza
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
