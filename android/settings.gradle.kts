29e54bfda88e8cde3a315f70e3337e9d1d439528
Alfredoooh <alfredopjonas@gmail.com>
2025-10-10 22:25:28 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
-    val flutterSdkPath = {
+    val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
         val flutterSdkPath = properties.getProperty("flutter.sdk")
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
-    }()
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -13,14 +13,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.gms.google-services") {
-                useModule("com.google.gms:google-services:4.4.0")
-            }
-        }
     }
 }
 
@@ -36,8 +28,9 @@ buildscript {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 include(":app")
