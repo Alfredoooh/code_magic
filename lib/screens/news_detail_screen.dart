f79a515bb61e2a0c9be5af4e6bb8371157be15f8
Alfredoooh <alfredopjonas@gmail.com>
2025-10-12 14:19:01 +0100

    Atualizar o news_detail_screen.dart

--- lib/screens/news_detail_screen.dart
+++ lib/screens/news_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'models/news_article.dart';
+import '../models/news_article.dart';
 
 class NewsDetailScreen extends StatefulWidget {
   final NewsArticle article;
