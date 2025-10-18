9083143eb5ec2fb0cf8264e3617ddadb311b293c
Alfredoooh <alfredopjonas@gmail.com>
2025-10-18 02:03:22 +0100

    Atualizar o wallet_card.dart

--- lib/widgets/wallet_card.dart
+++ lib/widgets/wallet_card.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:ui';
-import 'dart:math' as math;
 
 class WalletCard extends StatelessWidget {
   final Map<String, dynamic>? userData;
@@ -149,7 +147,7 @@ class WalletCard extends StatelessWidget {
     }
   }
 
-  // Aurora Card - Inspirado em American Express
+  // Aurora Card - Rosa/Vermelho suave
   Widget _buildAuroraCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -158,58 +156,25 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Color(0xFF6366F1).withOpacity(0.4),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Color(0xFFFF444F).withOpacity(0.2),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF4F46E5),
-                    Color(0xFF7C3AED),
-                    Color(0xFF6366F1),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: WavyLinesPainter(color: Colors.white.withOpacity(0.1)),
-            ),
-            Positioned(
-              top: -80,
-              right: -80,
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: RadialGradient(
-                    colors: [
-                      Colors.white.withOpacity(0.15),
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFFF444F),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
   }
 
-  // Ocean Card - Tons de azul/verde como Mastercard
+  // Ocean Card - Azul claro
   Widget _buildOceanCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -218,56 +183,25 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Color(0xFF0891B2).withOpacity(0.4),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Color(0xFF06B6D4).withOpacity(0.2),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0E7490),
-                    Color(0xFF0891B2),
-                    Color(0xFF06B6D4),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: CircularPatternPainter(color: Colors.white.withOpacity(0.08)),
-            ),
-            Positioned(
-              bottom: -50,
-              left: -50,
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.white.withOpacity(0.1),
-                    width: 40,
-                  ),
-                ),
-              ),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF06B6D4),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
   }
 
-  // Carbon Card - Preto com linhas texturizadas
+  // Carbon Card - Cinza escuro
   Widget _buildCarbonCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -276,58 +210,25 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.5),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0A0A0A),
-                    Color(0xFF1A1A1A),
-                    Color(0xFF0F0F0F),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: CarbonFiberPainter(),
-            ),
-            Positioned(
-              top: 20,
-              right: 20,
-              child: Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: RadialGradient(
-                    colors: [
-                      Color(0xFFFF444F).withOpacity(0.3),
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF1A1A1A),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
   }
 
-  // Sunset Card - Laranja/Rosa vibrante
+  // Sunset Card - Laranja suave
   Widget _buildSunsetCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -336,41 +237,25 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Color(0xFFF59E0B).withOpacity(0.4),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Color(0xFFF97316).withOpacity(0.2),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFF59E0B),
-                    Color(0xFFF97316),
-                    Color(0xFFEF4444),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: TopographicPainter(color: Colors.white.withOpacity(0.1)),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFF97316),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
   }
 
-  // Midnight Card - Azul escuro estrelado
+  // Midnight Card - Azul marinho
   Widget _buildMidnightCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -379,41 +264,25 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Color(0xFF1E3A8A).withOpacity(0.5),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Color(0xFF1E3A8A).withOpacity(0.3),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0F172A),
-                    Color(0xFF1E293B),
-                    Color(0xFF334155),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: StarfieldPainter(),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF1E3A8A),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
   }
 
-  // Emerald Card - Verde esmeralda com padrão
+  // Emerald Card - Verde esmeralda
   Widget _buildEmeraldCard(BuildContext context) {
     return Container(
       width: double.infinity,
@@ -422,52 +291,19 @@ class WalletCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Color(0xFF059669).withOpacity(0.4),
-            blurRadius: 25,
-            offset: Offset(0, 12),
+            color: Color(0xFF10B981).withOpacity(0.2),
+            blurRadius: 20,
+            offset: Offset(0, 8),
           ),
         ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF047857),
-                    Color(0xFF059669),
-                    Color(0xFF10B981),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            CustomPaint(
-              size: Size.infinite,
-              painter: HexagonPatternPainter(color: Colors.white.withOpacity(0.08)),
-            ),
-            Positioned(
-              bottom: -30,
-              right: -30,
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: RadialGradient(
-                    colors: [
-                      Colors.white.withOpacity(0.15),
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            _buildCardContent(context, Colors.white),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF10B981),
+          ),
+          child: _buildCardContent(context, Colors.white),
         ),
       ),
     );
@@ -528,10 +364,6 @@ class WalletCard extends StatelessWidget {
                     decoration: BoxDecoration(
                       color: Colors.white.withOpacity(0.2),
                       borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        color: Colors.white.withOpacity(0.3),
-                        width: 1,
-                      ),
                     ),
                     child: Icon(
                       CupertinoIcons.slider_horizontal_3,
@@ -552,10 +384,6 @@ class WalletCard extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.white.withOpacity(0.25),
                     borderRadius: BorderRadius.circular(6),
-                    border: Border.all(
-                      color: Colors.white.withOpacity(0.4),
-                      width: 1,
-                    ),
                   ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
@@ -618,164 +446,4 @@ class WalletCard extends StatelessWidget {
       return '0';
     }
   }
-}
-
-// Painters para padrões decorativos
-
-class WavyLinesPainter extends CustomPainter {
-  final Color color;
-  WavyLinesPainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = 1.5
-      ..style = PaintingStyle.stroke;
-
-    for (int i = 0; i < 8; i++) {
-      final path = Path();
-      final y = size.height * 0.15 * i;
-      path.moveTo(0, y);
-      
-      for (double x = 0; x <= size.width; x += 20) {
-        path.lineTo(x, y + math.sin(x * 0.05) * 8);
-      }
-      canvas.drawPath(path, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class CircularPatternPainter extends CustomPainter {
-  final Color color;
-  CircularPatternPainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = 1
-      ..style = PaintingStyle.stroke;
-
-    for (double r = 20; r < 300; r += 20) {
-      canvas.drawCircle(Offset(size.width * 0.7, size.height * 0.5), r, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class CarbonFiberPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.white.withOpacity(0.02)
-      ..style = PaintingStyle.fill;
-
-    for (double i = 0; i < size.width; i += 4) {
-      for (double j = 0; j < size.height; j += 4) {
-        if ((i + j) % 8 == 0) {
-          canvas.drawRect(Rect.fromLTWH(i, j, 2, 2), paint);
-        }
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class TopographicPainter extends CustomPainter {
-  final Color color;
-  TopographicPainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = 1
-      ..style = PaintingStyle.stroke;
-
-    for (int i = 0; i < 10; i++) {
-      final path = Path();
-      final yOffset = i * 25.0;
-      path.moveTo(0, yOffset);
-      
-      for (double x = 0; x <= size.width; x += 30) {
-        final y = yOffset + math.sin(x * 0.03 + i) * 15;
-        path.lineTo(x, y);
-      }
-      canvas.drawPath(path, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class StarfieldPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.fill;
-
-    final random = math.Random(42);
-    for (int i = 0; i < 50; i++) {
-      final x = random.nextDouble() * size.width;
-      final y = random.nextDouble() * size.height;
-      final starSize = random.nextDouble() * 2 + 0.5;
-      final opacity = random.nextDouble() * 0.5 + 0.3;
-      
-      paint.color = Colors.white.withOpacity(opacity);
-      canvas.drawCircle(Offset(x, y), starSize, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class HexagonPatternPainter extends CustomPainter {
-  final Color color;
-  HexagonPatternPainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = 1
-      ..style = PaintingStyle.stroke;
-
-    final hexSize = 30.0;
-    for (double y = 0; y < size.height + hexSize; y += hexSize * 1.5) {
-      for (double x = 0; x < size.width + hexSize; x += hexSize * math.sqrt(3)) {
-        final offset = (y / (hexSize * 1.5)) % 2 == 0 ? 0.0 : hexSize * math.sqrt(3) / 2;
-        _drawHexagon(canvas, Offset(x + offset, y), hexSize, paint);
-      }
-    }
-  }
-
-  void _drawHexagon(Canvas canvas, Offset center, double size, Paint paint) {
-    final path = Path();
-    for (int i = 0; i < 6; i++) {
-      final angle = (math.pi / 3) * i;
-      final x = center.dx + size * math.cos(angle);
-      final y = center.dy + size * math.sin(angle);
-      if (i == 0) {
-        path.moveTo(x, y);
-      } else {
-        path.lineTo(x, y);
-      }
-    }
-    path.close();
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
