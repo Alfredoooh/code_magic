c8ed7d26ab9c55b69108a528e8e583ba47925ae5
Alfredoooh <alfredopjonas@gmail.com>
2025-10-18 08:14:37 +0100

    Atualizar o post_card_screens.dart

--- lib/widgets/post_card_screens.dart
+++ lib/widgets/post_card_screens.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'dart:convert';
-import '../widgets/app_ui_components.dart';
+import 'app_ui_components.dart';
 import 'post_card.dart';
 
 class FullDescriptionScreen extends StatelessWidget {
