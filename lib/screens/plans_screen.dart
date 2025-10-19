7b98e3f01351238f65f3c10f3c5780a4eb67f76a
Alfredoooh <alfredopjonas@gmail.com>
2025-10-19 02:47:01 +0100

    Atualizar o plans_screen.dart

--- lib/screens/plans_screen.dart
+++ lib/screens/plans_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../widgets/app_ui_components.dart';
+import '../widgets/app_colors.dart';
 import '../services/plans_service.dart';
 import '../models/plan_model.dart';
 
