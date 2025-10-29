6928614fca15c6e6119fa3eae67b995cfb43b3bf
Alfredoooh <alfredopjonas@gmail.com>
2025-10-29 12:47:43 +0100

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,6 +8,13 @@ buildscript {
     }
 }
 
+plugins {
+    // declarar versões no root e aplicar false; módulos aplicam os plugins
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
