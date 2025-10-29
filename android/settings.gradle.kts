c70e5f47e7e73c089d21a187a57b811eaae6d492
Alfredoooh <alfredopjonas@gmail.com>
2025-10-29 12:59:32 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,10 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        val localPropertiesFile = file("local.properties")
-        if (localPropertiesFile.exists()) {
-            localPropertiesFile.inputStream().use { properties.load(it) }
-        }
-        val path = properties.getProperty("flutter.sdk")
-        requireNotNull(path) { "flutter.sdk not set in local.properties" }
-        path
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -19,13 +16,10 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
-rootProject.name = "madeeasy"
 include(":app")
