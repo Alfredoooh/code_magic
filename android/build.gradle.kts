21862b54c6f2278deba7ea88391c8221a9da10e9
Alfredoooh <alfredopjonas@gmail.com>
2025-10-29 13:00:05 +0100

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,21 @@
-buildscript {
+allprojects {
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-    }
 }
 
-plugins {
-    // declarar versões no root e aplicar false; módulos aplicam os plugins
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-}
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register("clean", Delete::class) {
+tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
