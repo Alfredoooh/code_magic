e9e50dc5daad2138ebd15c6244cae32aa4df0a37
Alfredoooh <alfredopjonas@gmail.com>
2025-10-29 12:39:44 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,6 +20,11 @@ pluginManagement {
 }
 
 plugins {
+    // Declare Android & Kotlin plugins here (apply false so modules can apply them)
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+
+    // Flutter plugin loader (kept as in your original)
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 }
 
@@ -33,3 +38,21 @@ dependencyResolutionManagement {
 
 rootProject.name = "madeeasy"
 include(":app")
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
