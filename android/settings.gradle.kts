2d53d71f2053b29899e5680c27c0c7af3b0d801c
Alfredoooh <alfredopjonas@gmail.com>
2025-10-29 12:10:46 +0100

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,36 +1,41 @@
 import java.util.Properties
+import java.io.FileInputStream
 
 pluginManagement {
-    // ler local.properties para obter flutter.sdk
-    val flutterSdkPath = run {
+    val flutterSdkPath: String by lazy {
         val properties = Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
+        val localProperties = file("local.properties")
+        if (localProperties.exists()) {
+            FileInputStream(localProperties).use { properties.load(it) }
+        }
+        val path = properties.getProperty("flutter.sdk")
+        requireNotNull(path) { "flutter.sdk not set in local.properties" }
+        path
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        // necessário para plugins (AGP, Kotlin plugin, etc)
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
 dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.initialization.dsl.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // necessário para dependências do Firebase/Google
         google()
         mavenCentral()
     }
 }
 
-// mantém o nome do root project (opcional)
-rootProject.name = "app"
-
-// inclui o módulo app
+rootProject.name = "zoomtrade"
 include(":app")
