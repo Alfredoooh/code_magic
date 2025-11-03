e1bc5b2447100f7a7a3089c7c1f4c099be5a053f
Alfredoooh <alfredopjonas@gmail.com>
2025-11-03 23:51:28 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'screens/splash_screen.dart';
@@ -84,7 +84,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/* app main
+*/
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -115,5 +115,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-*/
