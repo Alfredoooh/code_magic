832e81776fbf62f53ec390dedba5878c81b5ae87
Alfredoooh <alfredopjonas@gmail.com>
2025-11-04 02:20:43 +0100

    Atualizar o custom_icons.dart

--- lib/widgets/custom_icons.dart
+++ lib/widgets/custom_icons.dart
@@ -1,170 +1,262 @@
 // lib/widgets/custom_icons.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomIcons {
-  static const String home = 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z';
-  static const String users = 'M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z';
-  static const String plus = 'M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z';
-  static const String bell = 'M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z';
-  static const String menu = 'M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z';
-  static const String search = 'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z';
-  static const String inbox = 'M19 3H4.99c-1.11 0-1.98.89-1.98 2L3 19c0 1.1.88 2 1.99 2H19c1.1 0 2-.9 2-2V5c0-1.11-.9-2-2-2zm0 12h-4c0 1.66-1.35 3-3 3s-3-1.34-3-3H4.99V5H19v10z';
-  static const String marketplace = 'M21.9 8.89l-1.05-4.37c-.22-.9-1-1.52-1.91-1.52H5.05c-.9 0-1.69.63-1.9 1.52L2.1 8.89c-.24 1.02-.02 2.06.62 2.88.08.11.19.19.28.29V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6.94c.09-.09.2-.18.28-.28.64-.82.87-1.87.62-2.89zM18.91 4.99l1.05 4.37c.1.42.01.84-.25 1.17-.14.18-.44.47-.94.47-.61 0-1.14-.49-1.21-1.14L16.98 5l1.93-.01zM13 5h1.96l.54 4.52c.05.39-.07.78-.33 1.07-.22.26-.54.41-.95.41-.67 0-1.22-.59-1.22-1.31V5zM8.49 9.52L9.04 5H11v4.69c0 .72-.55 1.31-1.29 1.31-.34 0-.65-.15-.89-.41-.25-.29-.37-.68-.33-1.07zm-4.45-.16L5.05 5h1.97l-.58 4.86c-.08.65-.6 1.14-1.21 1.14-.49 0-.8-.29-.93-.47-.27-.32-.36-.75-.26-1.17zM5 19v-6.03c.08.01.15.03.23.03.87 0 1.66-.36 2.24-.95.6.6 1.4.95 2.31.95.87 0 1.65-.36 2.23-.93.59.57 1.39.93 2.29.93.84 0 1.64-.35 2.24-.95.58.59 1.37.95 2.24.95.08 0 .15-.02.23-.03V19H5z';
-  static const String settings = 'M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z';
-  static const String logout = 'M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z';
+  // Ícones atualizados com SVG mais limpos e modernos
+  static const String home = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+      <polyline points="9 22 9 12 15 12 15 22"/>
+    </svg>
+  ''';
+
+  static const String users = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+      <circle cx="9" cy="7" r="4"/>
+      <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+      <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+    </svg>
+  ''';
+
+  static const String plus = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3">
+      <line x1="12" y1="5" x2="12" y2="19"/>
+      <line x1="5" y1="12" x2="19" y2="12"/>
+    </svg>
+  ''';
+
+  static const String bell = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
+      <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
+    </svg>
+  ''';
+
+  static const String menu = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <line x1="3" y1="6" x2="21" y2="6"/>
+      <line x1="3" y1="12" x2="21" y2="12"/>
+      <line x1="3" y1="18" x2="21" y2="18"/>
+    </svg>
+  ''';
+
+  static const String search = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <circle cx="11" cy="11" r="8"/>
+      <path d="m21 21-4.35-4.35"/>
+    </svg>
+  ''';
+
+  static const String inbox = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"/>
+      <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"/>
+    </svg>
+  ''';
+
+  static const String marketplace = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="m5 11 4-7"/>
+      <path d="m19 11-4-7"/>
+      <path d="M2 11h20"/>
+      <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8c.9 0 1.8-.7 2-1.6l1.7-7.4"/>
+      <path d="M9 11v5"/>
+      <path d="M12 11v5"/>
+      <path d="M15 11v5"/>
+    </svg>
+  ''';
+
+  static const String settings = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <circle cx="12" cy="12" r="3"/>
+      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"/>
+    </svg>
+  ''';
+
+  static const String logout = '''
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
+      <polyline points="16 17 21 12 16 7"/>
+      <line x1="21" y1="12" x2="9" y2="12"/>
+    </svg>
+  ''';
 }
 
-class IconPainter extends CustomPainter {
-  final String pathData;
+class CustomIcon extends StatelessWidget {
+  final String svgString;
+  final double size;
   final Color color;
+  final BoxFit fit;
 
-  IconPainter(this.pathData, this.color);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.fill;
-
-    final path = parseSvgPath(pathData);
-    
-    final bounds = path.getBounds();
-    final scaleX = size.width / bounds.width;
-    final scaleY = size.height / bounds.height;
-    final scale = scaleX < scaleY ? scaleX : scaleY;
-    
-    final matrix = Matrix4.identity()
-      ..translate(
-        (size.width - bounds.width * scale) / 2 - bounds.left * scale,
-        (size.height - bounds.height * scale) / 2 - bounds.top * scale,
-      )
-      ..scale(scale);
-
-    canvas.drawPath(path.transform(matrix.storage), paint);
-  }
-
-  Path parseSvgPath(String pathData) {
-    final path = Path();
-    final commands = pathData.split(RegExp(r'(?=[MmLlHhVvCcSsQqTtAaZz])'));
-    
-    double currentX = 0;
-    double currentY = 0;
-    
-    for (var command in commands) {
-      if (command.isEmpty) continue;
-      
-      final type = command[0];
-      final coords = command.substring(1)
-          .trim()
-          .split(RegExp(r'[\s,]+'))
-          .where((s) => s.isNotEmpty)
-          .map((s) => double.tryParse(s) ?? 0)
-          .toList();
-      
-      switch (type) {
-        case 'M':
-          if (coords.length >= 2) {
-            currentX = coords[0];
-            currentY = coords[1];
-            path.moveTo(currentX, currentY);
-          }
-          break;
-        case 'm':
-          if (coords.length >= 2) {
-            currentX += coords[0];
-            currentY += coords[1];
-            path.moveTo(currentX, currentY);
-          }
-          break;
-        case 'L':
-          if (coords.length >= 2) {
-            currentX = coords[0];
-            currentY = coords[1];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'l':
-          if (coords.length >= 2) {
-            currentX += coords[0];
-            currentY += coords[1];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'H':
-          if (coords.isNotEmpty) {
-            currentX = coords[0];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'h':
-          if (coords.isNotEmpty) {
-            currentX += coords[0];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'V':
-          if (coords.isNotEmpty) {
-            currentY = coords[0];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'v':
-          if (coords.isNotEmpty) {
-            currentY += coords[0];
-            path.lineTo(currentX, currentY);
-          }
-          break;
-        case 'C':
-          if (coords.length >= 6) {
-            path.cubicTo(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]);
-            currentX = coords[4];
-            currentY = coords[5];
-          }
-          break;
-        case 'c':
-          if (coords.length >= 6) {
-            path.cubicTo(
-              currentX + coords[0],
-              currentY + coords[1],
-              currentX + coords[2],
-              currentY + coords[3],
-              currentX + coords[4],
-              currentY + coords[5],
-            );
-            currentX += coords[4];
-            currentY += coords[5];
-          }
-          break;
-        case 'S':
-          if (coords.length >= 4) {
-            path.quadraticBezierTo(coords[0], coords[1], coords[2], coords[3]);
-            currentX = coords[2];
-            currentY = coords[3];
-          }
-          break;
-        case 's':
-          if (coords.length >= 4) {
-            path.quadraticBezierTo(
-              currentX + coords[0],
-              currentY + coords[1],
-              currentX + coords[2],
-              currentY + coords[3],
-            );
-            currentX += coords[2];
-            currentY += coords[3];
-          }
-          break;
-        case 'Z':
-        case 'z':
-          path.close();
-          break;
-      }
-    }
-    
-    return path;
-  }
+  const CustomIcon({
+    Key? key,
+    required this.svgString,
+    this.size = 24.0,
+    this.color = Colors.black,
+    this.fit = BoxFit.contain,
+  }) : super(key: key);
 
   @override
-  bool shouldRepaint(IconPainter oldDelegate) =>
-      pathData != oldDelegate.pathData || color != oldDelegate.color;
+  Widget build(BuildContext context) {
+    return SvgPicture.string(
+      svgString,
+      width: size,
+      height: size,
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      fit: fit,
+    );
+  }
+}
+
+// Widgets específicos para cada ícone
+class HomeIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const HomeIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.home,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class UsersIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const UsersIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.users,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class PlusIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const PlusIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.plus,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class BellIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const BellIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.bell,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class MenuIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const MenuIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.menu,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class SearchIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const SearchIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.search,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class InboxIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const InboxIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.inbox,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class MarketplaceIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const MarketplaceIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.marketplace,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class SettingsIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const SettingsIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.settings,
+      size: size,
+      color: color,
+    );
+  }
+}
+
+class LogoutIcon extends StatelessWidget {
+  final double size;
+  final Color color;
+  const LogoutIcon({Key? key, this.size = 24.0, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      svgString: CustomIcons.logout,
+      size: size,
+      color: color,
+    );
+  }
 }
