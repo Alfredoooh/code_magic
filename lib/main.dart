48a67fcd344ac5999cb827679d310c35ab6c5bdd
Alfredoooh <alfredopjonas@gmail.com>
2025-11-06 18:33:50 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -100,36 +100,3 @@ class MyApp extends StatelessWidget {
     return const LoginScreen();
   }
 }
-
-/*
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'screens/webview_screen.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Modo edge-to-edge para o WebView ficar abaixo da status bar
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CashNet',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const WebViewScreen(),
-    );
-  }
-}
-*/
