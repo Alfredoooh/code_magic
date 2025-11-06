ac7459bec06e8910cfc8345e586bbf77bfb10ef1
Alfredoooh <alfredopjonas@gmail.com>
2025-11-06 19:26:26 +0100

    Atualizar o firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,18 +1,7 @@
-// lib/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
@@ -53,22 +42,25 @@ class DefaultFirebaseOptions {
     authDomain: 'delta-182c8.firebaseapp.com',
     storageBucket: 'delta-182c8.firebasestorage.app',
     measurementId: 'G-PCLWXJHCBT',
+    databaseURL: 'https://delta-182c8-default-rtdb.europe-west1.firebasedatabase.app',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyC5U9dqsap6vJY0MF1RWuYMt83b2ezb6t0',
-    appId: '1:329928312757:android:XXXXXXXXXXXXXXXX', // Substitua pelo App ID Android correto
+    apiKey: 'AIzaSyBLUfbKGwKX0XP8RRTvesa8_9x8zHA9V4A',
+    appId: '1:329928312757:android:4b16e327699c41b03abbfc',
     messagingSenderId: '329928312757',
     projectId: 'delta-182c8',
     storageBucket: 'delta-182c8.firebasestorage.app',
+    databaseURL: 'https://delta-182c8-default-rtdb.europe-west1.firebasedatabase.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyC5U9dqsap6vJY0MF1RWuYMt83b2ezb6t0',
-    appId: '1:329928312757:ios:XXXXXXXXXXXXXXXX', // Substitua pelo App ID iOS correto
+    appId: '1:329928312757:ios:XXXXXXXXXXXXXXXX',
     messagingSenderId: '329928312757',
     projectId: 'delta-182c8',
     storageBucket: 'delta-182c8.firebasestorage.app',
-    iosBundleId: 'com.example.printerlite', // Substitua pelo seu Bundle ID correto
+    iosBundleId: 'com.cahsnet.app',
+    databaseURL: 'https://delta-182c8-default-rtdb.europe-west1.firebasedatabase.app',
   );
 }
