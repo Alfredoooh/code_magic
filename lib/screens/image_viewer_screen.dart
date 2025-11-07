0c9d17bef2e786d171e79a255bda34e98f0fdb01
Alfredoooh <alfredopjonas@gmail.com>
2025-11-07 22:37:27 +0100

    Criar image_viewer_screen.dart

---
+++ lib/screens/image_viewer_screen.dart
@@ -0,0 +1,73 @@
+// lib/screens/image_viewer_screen.dart
+import 'package:flutter/material.dart';
+import '../services/image_service.dart';
+
+class ImageViewerScreen extends StatefulWidget {
+  final List<String> imageUrls;
+  final String initialUrl;
+
+  const ImageViewerScreen({
+    super.key,
+    required this.imageUrls,
+    required this.initialUrl,
+  });
+
+  @override
+  State<ImageViewerScreen> createState() => _ImageViewerScreenState();
+}
+
+class _ImageViewerScreenState extends State<ImageViewerScreen> {
+  late PageController _pageController;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.imageUrls.indexOf(widget.initialUrl);
+    if (_currentIndex == -1) _currentIndex = 0;
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: Text(
+          '${_currentIndex + 1} / ${widget.imageUrls.length}',
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: widget.imageUrls.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return InteractiveViewer(
+            panEnabled: true,
+            minScale: 0.5,
+            maxScale: 4.0,
+            child: Center(
+              child: ImageService.buildImageFromUrl(
+                widget.imageUrls[index],
+                fit: BoxFit.contain,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
