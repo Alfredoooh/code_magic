c6344f0e5b713cf62f927ca0407c4954b0d6eea5
Alfredoooh <alfredopjonas@gmail.com>
2025-11-09 08:59:02 +0100

    Atualizar o custom_icons.dart

--- lib/widgets/custom_icons.dart
+++ lib/widgets/custom_icons.dart
@@ -531,6 +531,12 @@ static const String folder = '''
   <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
 </svg>
 ''';
+
+  static const String arrowBack = '''
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+  <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" />
+</svg>
+''';
 }
 
 class SvgIcon extends StatelessWidget {
