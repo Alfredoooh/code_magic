373d8f0559a2e6ea944270f8f35e728f776f4626
Alfredoooh <alfredopjonas@gmail.com>
2025-11-12 15:45:50 +0100

    Atualizar o custom_icons.dart

--- lib/widgets/custom_icons.dart
+++ lib/widgets/custom_icons.dart
@@ -19,10 +19,8 @@ static const String pending = '''
 </svg>
 ''';
 
-static IconData get pendingIcon => _buildIcon(pending);
-
 // Ícone de título (title) - documento com linhas
-static const String title = '''
+   static const String title = '''
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
   <path d="M14 2v6h6" />
@@ -32,10 +30,8 @@ static const String title = '''
 </svg>
 ''';
 
-static IconData get titleIcon => _buildIcon(title);
-
 // Ícone de agendado (scheduled) - calendário com check
-static const String scheduled = '''
+   static const String schedule = '''
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
   <path d="M16 2v4" />
