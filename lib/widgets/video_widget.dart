bc20382623867ef501e79cbd5fcc21e57d279e27
Alfredoooh <alfredopjonas@gmail.com>
2026-01-12 19:52:26 +0100

    Delete lib directory

--- lib/widgets/video_widget.dart
+++
@@ -1,4 +0,0 @@
-// lib/widgets/video_widget.dart
-// Conditionally export platform implementation: mobile (io) by default, web when compiled for web.
-export 'video_widget_io.dart' 
-  if (dart.library.html) 'video_widget_web.dart';
