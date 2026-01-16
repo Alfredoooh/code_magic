30364cab50ae96a85eb4206331987a6dc9959b05
Alfredoooh <alfredopjonas@gmail.com>
2026-01-16 13:50:23 +0100

    Atualizar o app_header.dart

--- lib/widgets/app_header.dart
+++ lib/widgets/app_header.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ionicons/ionicons.dart';
-import '../svg_icons.dart';
 
 class AppHeader extends StatelessWidget {
   final VoidCallback onMenuTap;
@@ -9,6 +7,7 @@ class AppHeader extends StatelessWidget {
   final Color bgColor;
   final Color borderColor;
   final Color textColor;
+  final bool isDarkTheme;
 
   const AppHeader({
     Key? key,
@@ -17,12 +16,13 @@ class AppHeader extends StatelessWidget {
     required this.bgColor,
     required this.borderColor,
     required this.textColor,
+    required this.isDarkTheme,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: bgColor,
+      color: isDarkTheme ? bgColor : Colors.white,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: Row(
@@ -31,30 +31,27 @@ class AppHeader extends StatelessWidget {
             GestureDetector(
               onTap: onMenuTap,
               child: Container(
-                width: 32,
-                height: 32,
+                width: 36,
+                height: 36,
                 decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: borderColor,
+                  color: borderColor.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                child: Center(
-                  child: SvgPicture.string(
-                    SvgIcons.profileFilled,
-                    width: 18,
-                    height: 18,
-                    colorFilter: ColorFilter.mode(textColor, BlendMode.srcIn),
-                  ),
+                child: Icon(
+                  Ionicons.menu_outline,
+                  size: 22,
+                  color: textColor,
                 ),
               ),
             ),
             Image.asset(
               'assets/logo.png',
-              width: 32,
-              height: 32,
+              width: 36,
+              height: 36,
               errorBuilder: (context, error, stackTrace) {
                 return Container(
-                  width: 32,
-                  height: 32,
+                  width: 36,
+                  height: 36,
                   decoration: BoxDecoration(
                     color: const Color(0xFF2374E1),
                     borderRadius: BorderRadius.circular(8),
@@ -69,10 +66,18 @@ class AppHeader extends StatelessWidget {
             ),
             GestureDetector(
               onTap: onSearchTap,
-              child: SvgPicture.string(
-                SvgIcons.search,
-                width: 24,
-                height: 24,
+              child: Container(
+                width: 36,
+                height: 36,
+                decoration: BoxDecoration(
+                  color: borderColor.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(
+                  Ionicons.search_outline,
+                  size: 20,
+                  color: textColor,
+                ),
               ),
             ),
           ],
